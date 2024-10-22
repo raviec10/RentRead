6a5c4fb923f68163a807ad414b3518b71f26ccac
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-22 09:17:01 +0530

    Create Book.java

---
+++ src/main/java/entity/Book.java
@@ -0,0 +1,14 @@
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
+    private boolean availabilityStatus;
+}
