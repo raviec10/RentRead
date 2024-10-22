4136c7d7a400e40469a1c327191ce9a28851af95
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-22 09:07:04 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,17 @@
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
+
+public enum Role {
+    USER, ADMIN
+}
