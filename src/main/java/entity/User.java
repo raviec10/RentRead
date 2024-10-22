89d65917e08d86f649e289282ac625b3d20a0600
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-22 09:16:36 +0530

    Create User.java

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,21 @@
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
+
+public enum Role {
+    USER, ADMIN
+}
