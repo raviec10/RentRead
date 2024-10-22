f7c0dffbf4eb41c741ddc56ef3e4b4e86ad97c24
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-22 09:14:25 +0530

    Create UserController.java

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,22 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+}
